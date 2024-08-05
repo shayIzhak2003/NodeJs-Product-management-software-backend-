6e49d5fcdb136243fc0e65018b895f42c552f573
shay shalom izhak <alonizhakalonizhak@gmail.com>
2024-08-05 18:57:39 +0300

    added some changes

--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 
+const generateTimestamp = () => {
+  const now = new Date();
+  const date = now.toISOString().split('T')[0];
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  return date + "--" + hours + ":" + minutes;
+};
+
 const logRequest = (req, res, next) => {
-  const logEntry = `${new Date().toISOString()} - ${req.method} - ${req.url}\n`;
+  const logEntry = `${generateTimestamp()} - ${req.method} - ${req.url}\n`;
   fs.appendFileSync('logs/requests.log', logEntry);
   next();
 };
